b8be4b5579312b42a9dca35f70c049a4c90825b9
Omar <148300714+i3omr@users.noreply.github.com>
2025-03-01 08:24:49 +0000

    Volunteering System

---
+++ Application.java
@@ -0,0 +1,19 @@
+public class Application {
+    
+    private Volunteer volunteer;
+    private Opportunity opportunity;
+
+    public Application(Volunteer volunteer, Opportunity opportunity) {
+        this.volunteer = volunteer;
+        this.opportunity = opportunity;
+    }
+
+    public Volunteer getVolunteer() {
+        return volunteer;
+    }
+
+    public Opportunity getOpportunity() {
+        return opportunity;
+    }
+
+}
